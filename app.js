56c3d1237a0e07064e81274bd0fd87f01769d9ff
unknown <ckelly28@myseneca.ca>
2019-10-05 09:33:39 -0400

    added fonts, finished HTML for home

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const PORT = 3000;
 const express = require("express");
 const exphbs = require("express-handlebars");
-
+const bodyParser = require("body-parser")
 const app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
@@ -19,6 +20,14 @@ app.get('/roomList', (req, res)=>{
 
 app.get('/userReg', (req, res)=>{
     res.render('userReg')
+})
+
+app.post("/home", (req, res)=>
+{
+    const errors = [];
+    if(req.body.searchCity == "Toronto"){
+        res.redirect("/roomList");
+    }
 })
 
 app.listen(PORT, ()=>{
