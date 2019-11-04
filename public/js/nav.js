17751857288f2836554e109ddce1702c662bf3a6
ckelly2112 <ckelly28@myseneca.ca>
2019-11-03 19:30:29 -0500

    Removed a js file

--- public/js/nav.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById("navButton");
-
-menu.addEventListener("click", ()=>{
-    const nav = document.getElementById("responsiveNav");
-    nav.style.display = "block";
-})
