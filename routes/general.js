cc4ff79f3e2d91dec99df4ac6c280e9cf7a46462
ckelly2112 <ckelly28@myseneca.ca>
2019-12-08 16:28:16 -0500

    Updated search function and fixed deleting images

--- routes/general.js
+++ routes/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Room = require("../models/task")
 const result =[];
 
 
@@ -7,38 +8,19 @@ router.get('/', (req, res)=>{
     res.render('general/home')
 })
 
-router.get('/roomList', (req, res)=>{
-    if(result == "Toronto"){
-        res.render('task/roomList', {
-            Toronto: true,
-            city: result
+router.get('/viewRooms', (req,res)=>{
+    Room.find({roomLocation:result})
+    .then(room=>{
+        res.render('general/viewRooms',{
+            rooms:room
         })
-    } else if(result == "Montreal"){
-        res.render('task/roomList', {
-            Montreal: true,
-            city: result
-        })
-    }else if(result == "Vancouver"){
-        res.render('task/roomList', {
-            Vancouver: true,
-            city: result
-        })
-    }else if(result == "St. John's"){
-        res.render('task/roomList', {
-            StJohns: true,
-            city: result
-        })
-    } 
-    else
-    res.render('task/roomList', {
-        empty: true
     })
 })
 
 router.post("/", (req, res)=>
 {
     const errors = [];
-    if (req.body.checkInDate <= '2019-10-07'){
+    if (req.body.checkInDate <= Date.now()){
         errors.push("You can not select a day in the past!");
     }
     if (req.body.checkOutDate <= req.body.checkInDate){
@@ -54,7 +36,7 @@ router.post("/", (req, res)=>
     } else{
         result.pop();
         result.push(`${req.body.searchCity}`);
-        res.redirect('/roomList')
+        res.redirect('/viewRooms')
     }
 })
 module.exports = router;
